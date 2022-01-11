acbbd291c21caffad739df46d04045aee78c0c71
Abuzar-cpu <kerimovabuzer25@gmail.com>
2022-01-11 20:31:20 +0400

    little changes and corrections

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 let chances = 6;
+let wordList = [
+    "able",
+    "about",
+    "account",
+    "acid",
+    "across",
+    "act",
+    "addition",
+    "adjustment",
+    "advertisement",
+    "after",
+    "again",
+    "against",
+    "agreement",
+    "air",
+    "all",
+    "almost",
+    "among",
+    "amount",
+    "amusement",
+    "and",
+    "angle",
+    "angry",
+    "animal",
+    "answer",
+    "ant",
+    "any",
+    "apparatus",
+    "apple",
+    "approval",
+    "arch",
+];
 
 let letters = document.getElementsByClassName("letter");
 
+/*
+ *Adding onclick listener to all letters
+ */
 for (const letter of letters) {
     letter.setAttribute("onclick", "checkLetter(this)");
 }
 
-
-let randomWord = "HELLO".toUpperCase();
+/*
+ *Selecting random word from the list above
+ */
+let randomWord = wordList[Math.floor(Math.random() * 29)].toUpperCase();
+console.log(randomWord);
 
 let word = document.getElementById("word");
 
-for(let i = 0; i < randomWord.length; i++)
-{
+/*
+ *Adds underscores to give hint about word length
+ */
+for (let i = 0; i < randomWord.length; i++) {
     let lets = document.createElement("div");
     lets.innerText = "_";
     lets.style.margin = "10vh 3vw";
@@ -22,44 +62,53 @@ for(let i = 0; i < randomWord.length; i++)
 
 unders = document.getElementsByClassName("guessedLetter");
 
+/*
+ *Checks the letter that was clicked
+ * if the letter is incorrect checks if the user has lost or won
+ * if none is true, decrements the chances count and marks letter as incorrect
+ * then checks if the user won or lost
+ */
 function checkLetter(letterElement) {
     var letterInWord = false;
     var clickedLetter = letterElement.innerText.trim();
-    
+
     for (let i = 0; i < randomWord.length; i++) {
-        if(randomWord[i].trim() === clickedLetter.trim())
-        {
+        if (randomWord[i].trim() === clickedLetter.trim()) {
             letterElement.classList.add("correct");
             unders[i].innerText = clickedLetter;
             letterInWord = true;
         }
     }
-    if (!letterInWord)
-    {
-        letterElement.classList.add("wrong");
-        chances--;
-        document.getElementById("guessCount").innerText = "Remaining chances: " + chances;
+
+    if (!letterInWord) {
+        if (!userWon() && !(chances <= 0)) {
+            letterElement.classList.add("wrong");
+            chances--;
+            document.getElementById("guessCount").innerText = "Remaining chances: " + chances;
+        }
     }
-    if(userWon())
-    {
+    
+    if (userWon()) {
         document.getElementById("word").style.backgroundColor = "lime";
         document.getElementById("word").style.padding = "1vh 1vw";
         return;
     }
-    if(chances <= 0)
-    {
+    if (chances <= 0) {
         document.getElementById("word").style.color = "white";
         document.getElementById("word").style.backgroundColor = "red";
         document.getElementById("word").style.padding = "1vh 1vw";
         return;
     }
+    
 }
 
-userWon = () =>
-{
+/**
+ * Function to check if the user won
+ * User wins if all the underscores are replaced by letter
+ */
+userWon = () => {
     for (let underScore of unders) {
-        if(underScore.innerText == '_')
-        {
+        if (underScore.innerText == '_') {
             return false;
         }
     }
